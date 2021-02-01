05b2d2dfb2e13fe4a4f921dba238ed760d17c2e9
Zyter <60459687+remter@users.noreply.github.com>
2021-02-01 00:44:25 -0800

    Set up the backend

---
+++ Back-End/inventory/src/main/java/com/zyter/inventory/HelloController.java
@@ -0,0 +1,14 @@
+package com.zyter.inventory;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/test")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
